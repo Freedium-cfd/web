3f61bfd5414ac952454b966014e92cfa499ae9b0
Zhymabek Roman <robanokssamit@yandex.ru>
2023-08-25 09:21:59 +0600

    fix: bug fixes

--- test.py
+++ test.py
@@ -3,5 +3,8 @@ url_for_test = [
     "https://medium.com/@iamalexmathers/21-sentences-that-will-make-you-more-attractive-than-most-b4ee755ee6c5?source=explore---------0-83--------------------9b9b2ec7_ee6e_4cfb_bb69_db1451bfb3db-------15",
     "https://levelup.gitconnected.com/some-linux-commands-that-can-boost-your-work-efficiency-dramatically-9dc802a10618",
     "https://medium.com/bugs-that-bite/oops-medium-error-cached-in-google-a9b3b19bb8e0",
-    "https://leshchuk.medium.com/http-cache-on-rails-nginx-stack-950fee2f8eef"
+    "https://leshchuk.medium.com/http-cache-on-rails-nginx-stack-950fee2f8eef",
+    "https://medium.com/swlh/35-actionable-tips-to-grow-your-medium-blog-4e4017b89905",
+    "https://valeman.medium.com/benchmarking-neural-prophet-part-i-neural-prophet-vs-prophet-252990763468",
+    "https://freedium.cfd/https://valeman.medium.com/python-vs-r-for-time-series-forecasting-395390432598"
 ]
