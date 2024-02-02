d9bf1068cbfd043a44492433576b0b4057040559
ZhymabekRoman <robanokssamit@yandex.ru>
2024-02-02 06:03:48 +0600

    New reverse-proxy black list update

--- scripts/generate_caddy_rules.py
+++ scripts/generate_caddy_rules.py
@@ -21,7 +21,7 @@ for file in static_files:
     OUTPUT_RULES.append(file_template)
 
 
-ACCESS_DENIED_PATHS = ["graphql/websocket", "onboarding/*", "wp-*", ".env", "api*", "apple-touch-icon-precomposed.png", "rss.xml", ".git/*", "apple-touch-icon-120x120.png", "apple-touch-icon-120x120-precomposed.png", "apple-touch-icon-152x152.png", "apple-touch-icon-152x152-precomposed.png", ".well-known/*"]
+ACCESS_DENIED_PATHS = ["websocket", "cdn-cgi/rum", "graphql/websocket", "onboarding/*", "wp-*", ".env", "api*", "apple-touch-icon-precomposed.png", "rss.xml", ".git/*", "apple-touch-icon-120x120.png", "apple-touch-icon-120x120-precomposed.png", "apple-touch-icon-152x152.png", "apple-touch-icon-152x152-precomposed.png", ".well-known/*"]
 
 access_denied_paths_template = """
     handle_path /{{ file }} {
