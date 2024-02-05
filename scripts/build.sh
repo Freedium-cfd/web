58c3f645d75df0b25ad9f0f0a850ccbb3b79248c
ZhymabekRoman <robanokssamit@yandex.ru>
2024-02-05 19:20:57 +0600

    Update build script to production

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-# pip install nuitka==1.8
+# pip install nuitka==2.0.1
 # sudo apt install patchelf ccache -y
 # sudo /usr/sbin/update-ccache-symlinks
 # export PATH="/usr/lib/ccache:$PATH"
 
-python3 -m nuitka --standalone --nofollow-import-to=pytest --python-flag=nosite,-O,isolated --plugin-enable=anti-bloat,implicit-imports,data-files,pylint-warnings --warn-implicit-exceptions --warn-unusual-code --prefer-source-code --include-package=uvicorn.workers --verbose --show-modules --show-memory --show-progress --show-scons server  # --low-memory
+python3 -m nuitka --standalone --nofollow-import-to=pytest --python-flag=nosite,-O,isolated --plugin-enable=anti-bloat,implicit-imports,data-files,pylint-warnings --warn-implicit-exceptions --warn-unusual-code --prefer-source-code --include-package=uvicorn.workers --include-package=sentry_sdk.integrations server
+
