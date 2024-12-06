86a200542951b1a8254dc46827444c9bb890d720
ZhymabekRoman <robanokssamit@yandex.ru>
2024-12-06 23:20:34 +0500

    fix(api): uvicorn run command path

--- freedium-library/src/freedium_library/api/main.py
+++ freedium-library/src/freedium_library/api/main.py
@@ -2,4 +2,4 @@ import uvicorn
 
 
 def start_server(host: str = "0.0.0.0", port: int = 7080) -> None:
-    uvicorn.run("freedium_library.api.app", host=host, port=port)
+    uvicorn.run("freedium_library.api.app:app", host=host, port=port)
